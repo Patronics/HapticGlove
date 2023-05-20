ee949ed75bd393dc40988162d20f4a8bd9822adf
JohnComputerScience <48074600+JohnComputerScience@users.noreply.github.com>
2023-05-20 16:55:41 -0700

    added motor pwm functionality

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import board
 import busio
 import digitalio
 import adafruit_vl53l0x
-
+import pwmio
 
 
 
@@ -13,6 +13,7 @@ distSense = adafruit_vl53l0x.VL53L0X(i2c)
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
+motorPin = pwmio.PWMOut(board.GP29)
 
 while True:
 	led.value = True
@@ -20,3 +21,10 @@ while True:
 	led.value = False
 	time.sleep(0.5)
 	print('Range: {}mm'.format(distSense.range))
+
+	normDist = dist.Sense.range / 1211	
+	dutyCycle = (1 - normDistance) * 65535
+	motorPin.duty_cycle = dutyCycle
+	print(dutyCycle)
+
+
