b1c6b037dc4545be232f7d0bf54df61d1db6dba9
Patrick Leiser <patrick27leiser@yahoo.com>
2023-05-20 19:57:16 -0700

    timing and max range tweak

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import adafruit_vl53l0x
 import pwmio
 import math
 
-maxDistVal = 1211 #max recorded valid value so far
+maxDistVal = 1270 #max recorded valid value so far
 
 i2c = busio.I2C(board.GP21, board.GP20)
 distSense = adafruit_vl53l0x.VL53L0X(i2c)
@@ -14,13 +14,13 @@ distSense = adafruit_vl53l0x.VL53L0X(i2c)
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
-motorPin = pwmio.PWMOut(board.GP22)
+motorPin = pwmio.PWMOut(board.GP0)
 
 while True:
 	led.value = True
-	time.sleep(0.5)
+	time.sleep(0.1)
 	led.value = False
-	time.sleep(0.5)
+	time.sleep(0.1)
 	print('Range: {}mm'.format(distSense.range))
 	
 	# distSense is ALWAYS 0 to 8192
