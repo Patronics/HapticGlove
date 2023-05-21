a5f56d33c59449521d493d99f8c25b41b5145594
Patrick Leiser <patrick27leiser@yahoo.com>
2023-05-21 09:23:13 -0700

    add potentiometer for adjusting maximum cutoff range

--- main.py
+++ main.py
@@ -2,17 +2,23 @@ import time
 import board
 import busio
 import digitalio
-import adafruit_vl53l0x
+import analogio
 import pwmio
+import adafruit_vl53l0x
 import math
 
 maxDistVal = 1270 #max recorded valid value so far
+minPotVal = 300
+maxPotVal = 65535
+
 
 i2c = busio.I2C(board.GP21, board.GP20)
 distSense = adafruit_vl53l0x.VL53L0X(i2c)
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
+
+pot = analogio.AnalogIn(board.GP26_A0)
 
 motorPin = pwmio.PWMOut(board.GP16)
 
@@ -23,14 +29,22 @@ while True:
 	time.sleep(0.1)
 	print('Range: {}mm'.format(distSense.range))
 	
+
 	# distSense is ALWAYS 0 to 8192
 	distSenseRange = distSense.range
-	if (distSenseRange > maxDistVal):
+	potVal = pot.value
+	if (potVal < minPotVal):
+		potVal = 0
+	else:
+		potVal = potVal/maxPotVal
+	
+	if (distSenseRange > (maxDistVal*potVal)):
 		distSenseRange = maxDistVal
-
+	
+	print('Pot: {}'.format(potVal))
 	normDist = distSenseRange / maxDistVal
 	dutyCycle = (1 - normDist) * 65535
 	motorPin.duty_cycle = math.floor(dutyCycle)
-	print(dutyCycle)
+	print('Duty Cycle: {}'.format(dutyCycle))
 
 
